b755a8a7ed316dfdc7ad221198eb8187f489b239
Bryan Mulvihill <bmulvihill@pinsonault.com>
2014-09-07 15:36:58 -0400

    coding algorithm

--- lib/apriori.rb
+++ lib/apriori.rb
@@ -1,24 +1,43 @@
 class Apriori
-  attr_reader :min_frequency, :set
+  attr_reader :min_support, :min_confidence, :data_set
+  attr_accessor :list
 
-  def initialize(min_frequency, set)
-    @min_frequency = min_frequency
-    @set = set
+  def initialize(data_set, min_support, min_confidence=0)
+    @data_set = data_set
+    @min_support = min_support
+    @min_confidence = min_confidence
+    @size = 0
+    @list = create_new_set(convert_initial_data_set)
   end
 
   def mine
-    count_frequency(set)
+    #list = create_new_set(data_set)
+    #candidates = count_frequency (data_set)
+    #remove_candidates(candidates)
   end
 
-  def count_frequency candidates
+  def count_frequency set
     list = {}
-    candidates.each do |transaction, items|
-      items.uniq.each do |item|
-        list[item.to_sym] ||= 0
-        list[item.to_sym] += 1
+    data_set.each do |transaction, items|
+      set.each do |set_items|
+        list[set_items.join(',')] ||= 0
+        list[set_items.join(',')] += 1 unless (items & set_items).empty?
       end
     end
     list
   end
 
+  def retrieve_candidates list
+    list.reject!{|key, value| (value.to_f/data_set.count) * 100 < min_support}.keys
+  end
+
+  def create_new_set candidates
+    @size += 1
+    candidates.combination(@size).to_a
+  end
+
+  private
+  def convert_initial_data_set
+    @data_set.values.flatten.uniq
+  end
 end
--- spec/apriori/apriori_spec.rb
+++ spec/apriori/apriori_spec.rb
@@ -10,20 +10,54 @@ describe Apriori do
   end
   context '#count_frequency' do
     it 'creates a hash of items and their count' do
-      apriori = Apriori.new(10, sample_data)
-      expect(apriori.count_frequency(sample_data)).to eql({
-        :Mango => 3,
-        :Onion => 3,
-        :Nintendo => 2,
-        :Keychain => 5,
-        :Eggs => 4,
-        :Yoyo => 3,
-        :Doll => 1,
-        :Apple => 1,
-        :Umbrella => 1,
-        :Corn => 2,
-        :Icecream => 1
+      apriori = Apriori.new(sample_data,10)
+      expect(apriori.count_frequency(apriori.list)).to eql({
+        'Mango' => 3,
+        'Onion' => 3,
+        'Nintendo' => 2,
+        'Keychain'=> 5,
+        'Eggs' => 4,
+        'Yoyo' => 3,
+        'Doll' => 1,
+        'Apple' => 1,
+        'Umbrella' => 1,
+        'Corn' => 2,
+        'Icecream' => 1
         })
+    end
+  end
+
+  context '#retrieve_candidates' do
+    it 'retrieves all candidates that meet minimum support' do
+      apriori = Apriori.new(sample_data, 50)
+      list = apriori.count_frequency(apriori.list)
+      expect(apriori.retrieve_candidates(list)).to eql([
+        'Mango',
+        'Onion',
+        'Keychain',
+        'Eggs',
+        'Yoyo'
+        ])
+    end
+  end
+
+  context '#create_new_list' do
+    it 'creates a new list of candidates' do
+      apriori = Apriori.new(sample_data, 50)
+      list = apriori.count_frequency(apriori.list)
+      candidates = apriori.retrieve_candidates(list)
+      expect(apriori.create_new_set(candidates)).to eql([
+        ['Mango', 'Onion'],
+        ['Mango', 'Keychain'],
+        ['Mango', 'Eggs'],
+        ['Mango', 'Yoyo'],
+        ['Onion', 'Keychain'],
+        ['Onion', 'Eggs'],
+        ['Onion', 'Yoyo'],
+        ['Keychain', 'Eggs'],
+        ['Keychain', 'Yoyo'],
+        ['Eggs', 'Yoyo']
+        ])
     end
   end
 
