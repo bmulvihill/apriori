fb9a9c29e9cdb7511e6e066fdda3c18789c0f161
Bryan Mulvihill <bmulvihill@pinsonault.com>
2014-09-08 21:16:28 -0400

    working on pruning

--- lib/apriori.rb
+++ lib/apriori.rb
@@ -5,7 +5,6 @@ class Apriori
     @data_set = data_set
     @min_support = min_support
     @min_confidence = min_confidence
-    self.iteration = 0
     self.list = create_new_list(convert_initial_data_set)
   end
 
@@ -41,17 +40,32 @@ class Apriori
   end
 
   def retrieve_candidates new_list
-    new_list.reject{|key, value| (value.to_f/data_set.count) * 100 < min_support}.keys
+    new_list = new_list.reject{|key, value| (value.to_f/data_set.count) * 100 < min_support}
+    new_list.keys
   end
 
+  def iterate
+    @iteration ||= 0
+    @iteration += 1
+  end
+
+  # need to implement self join here
   def create_new_list candidates
-    self.iteration = self.iteration + 1
-    candidates.map{|c| c.split(',')}.flatten.uniq.combination(self.iteration).to_a
+    iterate
+    candidates = prune candidates if iteration > 2
+    candidates.map{|c| c.split(',')}.flatten.uniq.combination(iteration).to_a
+  end
+
+  def prune candidates
+    c = candidates.map{|c| c.split(',')}
+    c = c.map{|candidate| candidate[0...-1]}.flatten #flatten array of pairs
+    c = c.inject(Hash.new(0)) {|item,count| item[count]+=1;item} # count occurances of each item
+    candidates.reject{|candidate| c[candidate.split(',')[0...-1].join(',')] < 2 } # removes those items with less than 2
   end
 
   private
-  attr_writer :list, :matching_rules, :iteration
-  
+  attr_writer :list, :matching_rules
+
   def convert_initial_data_set
     @data_set.values.flatten.uniq
   end
